bb8ea94321b410e521a8f72f7903152f9d4bf788
Louis Pachas <you@example.com>
2020-06-02 02:17:00 -0700

    staticroot

--- web_project/settings.py
+++ web_project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'this_is_it/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static_root')
 
 
 STATICFILES_DIRS = [
