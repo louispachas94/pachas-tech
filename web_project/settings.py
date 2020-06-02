f07fbdf09426d6f43238c259b7295f94df6d8abe
Louis Pachas <you@example.com>
2020-06-01 23:59:10 -0700

    database

--- web_project/settings.py
+++ web_project/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+if DEBUG:
 
-DATABASES = {
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pachastech',
+        'USER': 'pachasuser',
+        'PASSWORD': 'Rocky408',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -122,4 +134,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
